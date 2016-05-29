cbf355a4514aab17cb9bee13350575b8505abc3a
Ricardo Vladimiro <ricardo.vladimiro@miniclip.com>
2016-05-29 11:57:09 +0100

    Updated Clear() description

--- R/consoleClear.R
+++ R/consoleClear.R
@@ -1,6 +1,7 @@
 #' @export
 #' @name Clear
 #' @title Clear the console because I always forget the proper escape code
+#' @description I'm only writing a description to avoid a warning. I'm a nice package creator. Just type Clear() and the console will be cleared!
 #' @author Ricardo Vladimiro
 Clear <- function() {
     cat("\014") 
